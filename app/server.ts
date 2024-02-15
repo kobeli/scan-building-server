0571fee72bec09b0a423e6d6e9172ebeaa19d430
kobe <kobe@neuron.sg>
2024-02-15 22:04:00 +0800

    [Kobe] #N/A: Change "aws-sdk" to "@aws-sdk/client-s3".

--- app/server.ts
+++ app/server.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
 import * as process from 'process';
-// import AWS from 'aws-sdk'
 
 // 根据 NODE_ENV 加载对应的 .env 文件
 dotenv.config({
@@ -13,10 +12,9 @@ import { applyPhotoRoutes } from './routes/photo.routes';
 // import { db } from './models';
 import * as fs from 'fs';
 import * as path from 'path';
-import { ManagedUpload } from 'aws-sdk/lib/s3/managed_upload';
 import multer from 'multer';
-import SendData = ManagedUpload.SendData;
 import { getJson } from 'serpapi';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 const app = express();
 
@@ -31,7 +29,6 @@ app.get('/', (req, res) => {
   res.json({ message: '欢迎访问卡拉云后端服务器' });
 });
 
-/*
 // 配置 Multer
 const upload = multer({
   storage: multer.memoryStorage(), // 使用内存存储，文件将保存在内存中
@@ -55,25 +52,51 @@ app.post('/upload', upload.single('building'), function (req, res: any) {
   };
 
   // 上传文件到S3
-  const s3 = new AWS.S3();
-  s3.upload(params, function (err: Error, data: SendData) {
-    if (err) {
-      console.error('上传到S3出错: ', err);
-      return res.status(500).send('上传文件出错。');
-    }
-    // 成功上传到S3后，返回URL
-    const s3AddressUrl = data.Location;
-    if (!!s3AddressUrl) {
-      getJson({
-        engine: "google_lens",
-        api_key: '0fe2e55cbc9ca201746be8ed7b9f3f803dd12cbd0461100e599b458218bf897d', // Get your API_KEY from https://serpapi.com/manage-api-key
-        url: s3AddressUrl,
-      }, (json) => {
-        console.log(json);
-        res.send({ url: (json['knowledge_graph']) });
-      });
-    }
+  const s3 = new S3Client({
+    region: 'ap-southeast-1',
+    credentials: {
+      accessKeyId: 'AKIAQ3EGQXMPD5U3VQCC',
+      secretAccessKey: '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'
+    },
   });
+  const keyName = Date.now() + '_' + path.basename(file.originalname)
+  const command = new PutObjectCommand({
+    Bucket: "scanbuilding",
+    Key: keyName, // 文件名
+    Body: file.buffer,
+  });
+
+  s3.send(command).then((response) => {
+    const s3Url = `https://${'scanbuilding'}.s3.amazonaws.com/${encodeURIComponent(keyName)}`;
+    getJson({
+      engine: "google_lens",
+      api_key: '0fe2e55cbc9ca201746be8ed7b9f3f803dd12cbd0461100e599b458218bf897d', // Get your API_KEY from https://serpapi.com/manage-api-key
+      url: s3Url,
+    }, (json) => {
+      console.log(json);
+      res.send({ url: (json['knowledge_graph']) });
+    });
+  });
+
+  //
+  // s3.upload(params, function (err: Error, data: SendData) {
+  //   if (err) {
+  //     console.error('上传到S3出错: ', err);
+  //     return res.status(500).send('上传文件出错。');
+  //   }
+  //   // 成功上传到S3后，返回URL
+  //   const s3AddressUrl = data.Location;
+  //   if (!!s3AddressUrl) {
+  //     getJson({
+  //       engine: "google_lens",
+  //       api_key: '0fe2e55cbc9ca201746be8ed7b9f3f803dd12cbd0461100e599b458218bf897d', // Get your API_KEY from https://serpapi.com/manage-api-key
+  //       url: s3AddressUrl,
+  //     }, (json) => {
+  //       console.log(json);
+  //       res.send({ url: (json['knowledge_graph']) });
+  //     });
+  //   }
+  // });
 });
 
 
@@ -81,7 +104,7 @@ app.get('/form', function (req, res, next) {
   const form = fs.readFileSync(path.join(__dirname, 'form.html'), { encoding: 'utf8' });
   res.send(form);
 });
-*/
+
 applyPhotoRoutes(app);
 
 // 设置监听端口
